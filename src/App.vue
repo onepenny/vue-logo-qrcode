a79b180626e8cfa9a6e44468cbd808f7c91dc127
zhengjianjun <c512319@163.com>
2019-11-18 14:45:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <vueLogoQrcode ref="qrcode"
+                       :content="content"
+                       :logoSrc="logoSrc"
+                       :width="width"
+                       :autoGen="true"
+                       @click.native="downloadImage"></vueLogoQrcode>
+    </div>
+</template>
+
+<script>
+import vueLogoQrcode from './lib';
+import logoSrc from './assets/logo.png'
+
+
+export default {
+    name: 'app',
+    components: { vueLogoQrcode },
+    data() {
+        return {
+            content: "shaojiasong",
+            width: 200,
+            logoSrc,
+        }
+    },
+    methods: {
+        downloadImage() {
+            this.$refs.qrcode.genQrCodeImageDownload();
+        }
+    },
+  created(){
+      console.log(1)
+
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
